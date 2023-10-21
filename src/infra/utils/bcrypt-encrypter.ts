d26e680d18080ef428dfaebf84f139e8a2d2a5ef
Vitor Vaz Andrade <vitorandrade@ecomp.uefs.br>
2023-10-21 11:33:55 -0300

    feat: create infra layer and implements bcrypt encrypter

---
+++ src/infra/utils/bcrypt-encrypter.ts
@@ -0,0 +1,16 @@
+import Encrypter from "../../application/contracts/utils/encrypter";
+import bcrypt from 'bcrypt'
+
+export default class BcryptEncrypt implements  Encrypter{
+
+    async encrypt(token: string): Promise<string>{
+        const encrypted_token = await bcrypt.hash(token, 12)
+        return encrypted_token
+    }
+
+    async check(token: string, encripted_token: string): Promise<boolean>{
+        const is_valid = await bcrypt.compare(token, encripted_token)
+        return is_valid
+    }
+
+}
